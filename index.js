f819da906ebcc017b7c782528671455b2ade56cc
TRI-Xploit <79724265+TRI-Xploit@users.noreply.github.com>
2021-02-27 17:14:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ const { ind } = require('./language')
 /********** MENU SETTING **********/
 const vcard = 'BEGIN:VCARD\n' 
             + 'VERSION:3.0\n' 
-            + 'FN:Affis Admin\n' 
+            + 'FN:TRI-Xploit Admin\n' 
             + 'ORG: Pengembang XBot;\n' 
-            + 'TEL;type=CELL;type=VOICE;waid=6282334297175:+62 823-3429-7175\n' 
+            + 'TEL;type=CELL;type=VOICE;waid=6283140691740:+62 831-4069-1740\n' 
             + 'END:VCARD' 
 blocked = []   
 prefix = '#'
@@ -52,7 +52,7 @@ cr = '*BOT INI SUDAH TERVERIFIKASI*'
 /*************************************/
 
 /******** OWNER NUMBER**********/
-const ownerNumber = ["62895710073737@s.whatsapp.net","6282334297175@s.whatsapp.net"] 
+const ownerNumber = ["6283140691740@s.whatsapp.net","6283140691740@s.whatsapp.net"] 
 /************************************/
 
        
